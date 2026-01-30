13db4c08d5a03f821402707f54d33c086adb2dd4
Tobias Nodland <tobias_olsen02@hotmail.com>
2026-01-30 17:36:49 +0100

    C: begin parallell array to track used values

--- C/list/main.c
+++ C/list/main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct int32List int32List;
 
@@ -10,8 +11,13 @@ struct int32List
     int32_t capacity;
     int32_t *items;
     int32_t reallocChuck;
+    bool *usedIndex;
 
     bool (*add)(int32List *self, int32_t value);
+    int32_t (*get)(int32List *self, uint32_t index);
+    bool (*insert)(int32List *self, uint32_t index, int32_t value);
+    bool (*fill)(int32List *self, int32_t value);
+    bool (*remove)(int32List *self, uint32_t index);
 };
 
 bool int32ListAdd (int32List *self, int32_t value)
@@ -22,38 +28,97 @@ bool int32ListAdd (int32List *self, int32_t value)
         self->length++;
         return true;
     }
+    int32_t oldCap = self->capacity;
+    int32_t newSize = oldCap + self->reallocChuck;
 
-    int32_t newSize = self->capacity + self->reallocChuck;
-    int32_t *temp = realloc(self->items, sizeof(int32_t) * newSize);
-    if (temp == nullptr)
+
+    int32_t *ItemsTemp = realloc(self->items, sizeof(int32_t) * (size_t)newSize);
+    bool *UsedTemp = realloc(self->usedIndex, sizeof(bool) * (size_t)newSize);
+    if (ItemsTemp == nullptr || UsedTemp == nullptr)
     {
         fprintf(stderr,"Memory allocation failed\n");
         return false;
     }
-    self->items = temp;
+    self->usedIndex = UsedTemp;
+    self->items = ItemsTemp;
+    self->capacity = newSize;
+
+    memset(self->usedIndex + oldCap, false, (size_t)self->capacity * sizeof(bool)); //replace garbage with false
+    memset(self->items + oldCap, 0, (size_t)self->capacity  * sizeof(int32_t)); //replace garbage with 0
 
     self->items[self->length] = value;
     self->length++;
-    self->capacity = newSize;
 
+    return true;
+}
+
+int32_t int32ListGet(const int32List *self, uint32_t index)
+{
+    if (index >= self->capacity)
+    {
+        fprintf(stderr, "ERROR: Accessing out of bounds\n\tBounds of list: %d\n\tIndex of attempted to access: %d\n",
+            self->capacity, index);
+        return false;
+    }
+    if (index >= self->length)
+    {
+        fprintf(stderr, "ERROR: Accessing unused values\n\tLength of list: %d\n\tIndex of attempted access: %d\n",
+            self->length, index);
+        return false;
+    }
+
+    return self->items[index];
+}
+
+bool int32ListInsert(int32List *self, uint32_t index, int32_t value)
+{
+    if (index >= self->capacity)
+    {
+        fprintf(stderr, "ERROR: Inserting out of bounds\n\tBounds of list: %d\n\tIndex of attempted to insert: %d\n",
+            self->capacity, index);
+        return false;
+    }
+
+    self->items[index] = value;
+    if (index > self->length)
+    {
+        self->length += ((int32_t)index - self->length + 1);
+    }
+    return true;
+}
+
+bool int32ListFill(int32List *self, int32_t value)
+{
+    return true;
+}
+
+bool int32ListRemove(int32List *self, uint32_t index)
+{
     return true;
 }
 
 bool int32ListInit(int32List *self, int32_t Capacity)
 {
     self->add = int32ListAdd;
+    self->get = int32ListGet;
+    self->insert = int32ListInsert;
+    self->fill = int32ListFill;
+    self->remove = int32ListRemove;
 
     self->length = 0;
     self->capacity = Capacity;
     self->reallocChuck = 25;
 
-    self->items = calloc(Capacity, sizeof(int32_t));
-    if (self->items == nullptr)
+    self->items = calloc((size_t)Capacity, sizeof(int32_t));
+    self->usedIndex  = calloc((size_t)Capacity, sizeof(bool));
+    if (!self->items || !self->usedIndex)
     {
-        setbuf(stdout, nullptr);
+        free(self->items);
+        free(self->usedIndex);
         fprintf(stderr, "Memory not allocated!\n");
         return false;
     }
+    memset(self->usedIndex, false, (size_t)self->capacity * sizeof(bool));
 
     return true;
 }
@@ -64,21 +129,32 @@ int main(void)
     int32ListInit(&list, 4);
     setbuf(stdout, nullptr);
     printf("Initial Cap: %d\n", list.capacity);
-    list.add(&list, 3), list.add(&list, 2), list.add(&list, 1323);
+    list.add(&list, 3), list.add(&list, 2), list.add(&list, 1323), list.add(&list, 343),
+    list.add(&list, 43343);
 
     if (!list.add(&list, 653))
     {
         fprintf(stderr, "Value not added\n");
     }
 
-    for (int i = 0; i < list.capacity; ++i)
+    list.insert(&list,28, 540);
+
+    for (int i = 0; i < list.length; ++i)
     {
-        printf("List item at index %d: %d\n", i, list.items[i]);
+        printf("List item at index %d: %d\n", i, list.get(&list, i));
     }
+    for (int i = 0; i < list.length; ++i)
+    {
+        printf("Is index %d used: %hhd\n", i, list.usedIndex[i]);
+    }
+
+    list.get(&list, 600);
+    list.get(&list, 29);
 
     printf("Current Cap: %d\n", list.capacity);
     printf("length: %d\n", list.length);
     printf("Size: %llu\n", sizeof(int32_t) * (size_t)list.capacity);
+    printf("First val: %d\n", list.get(&list, 0));
 
     return 0;
 }
