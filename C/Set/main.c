9fdb24d1fe5a19ea66bdc37d09b59b0b832e0547
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-02-11 00:30:31 +0100

    c: simple int set

--- C/Set/main.c
+++ C/Set/main.c
@@ -1,43 +1,76 @@
 ﻿#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-typedef struct int32Set Int32Set;
+typedef struct Int32Set Int32Set;
 
-struct int32Set
+struct Int32Set
 {
     int *data;
-    size_t size;
     size_t capacity;
     int32_t (*get)(const Int32Set *set, int32_t index);
+    void (*add)(const Int32Set *set, int32_t value);
+    bool (*contains)(const Int32Set *set, int32_t value);
 };
 
-int32_t int32SetGet(const Int32Set *self, int32_t index)
+int32_t int32SetGet(const Int32Set *self, int32_t value)
 {
-    if (index >= self->capacity || index < 0)
+    if (value >= self->capacity || value < 0)
     {
-        fprintf(stderr,"index out of range\n");
+        fprintf(stderr,"ERROR: index %d out of range\n", value);
     }
 
-    if (self->size <= index)
-    {
-        fprintf(stderr,"Accessing unused values\n");
-    }
-
-    return self->data[index];
+    return self->data[value];
 }
 
-void int32SetInit(Int32Set *self, int32_t *buf, int32_t capacity )
+void int32SetAdd(const Int32Set *self, int32_t value)
 {
-    self->data = buf;
-    self->size = 0;
-    self->capacity = capacity;
-    memset(self->data, 0, capacity * sizeof(int32_t));
+    if (self->data[value] == value)
+    {
+        fprintf(stderr,"ERROR: value %d already exists\n", value);
+        return;
+    }
+
+    self->data[value] = value;
+}
+
+bool int32SetContains(const Int32Set *self, int32_t value)
+{
+    if (value >= self->capacity || value < 0)
+    {
+        fprintf(stderr,"ERROR: index %d out of range\n", value);
+        return false;
+    }
+
+    if (self->data[value] == value)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void int32SetInit(Int32Set *self)
+{
+    self->data = calloc(INT32_MAX, sizeof(int32_t));
+    self->capacity = INT32_MAX;
+    memset(self->data, 0, self->capacity * sizeof(int32_t));
+
     self->get = int32SetGet;
+    self->add = int32SetAdd;
+    self->contains = int32SetContains;
 }
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    Int32Set set;
+    int32SetInit(&set);
+
+    set.add(&set, 5);
+    set.add(&set, 10);
+    set.add(&set, 20);
+    set.add(&set, 20);
+
     return 0;
 }
