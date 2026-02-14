438a407f2a7eacfefb1be2732c93ff93574bbbee
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-02-14 02:14:37 +0100

    c: begin set

---
+++ C/map/main.c
@@ -0,0 +1,35 @@
+﻿#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct map map;
+
+struct map
+{
+    int32_t *keys;
+    char *values;
+    int64_t size;
+
+    void (*add)(map *self, int32_t key, char value);
+};
+
+void mapAdd(map *self, int32_t key, char value)
+{
+
+}
+
+void mapInit (map *self, int64_t size)
+{
+    self->keys = malloc(sizeof(int32_t) * size);
+    self->values =  malloc(sizeof(char) * size);
+    self->size = size;
+
+    self->add = mapAdd;
+}
+
+int main(void)
+{
+    map map;
+    mapInit(&map, 10);
+    return 0;
+}
