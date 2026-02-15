4acd78ce90910667ba2c3870f638251bf8756624
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-02-15 22:35:44 +0100

    c: map add

--- C/map/main.c
+++ C/map/main.c
@@ -2,23 +2,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct mapElement
+{
+    int32_t key;
+    char value;
+    bool wasInserted;
+} mapElement;
+
 typedef struct map map;
 
 struct map
 {
     int32_t *keys;
     char *values;
-    int64_t size;
+    int32_t size;
 
-    void (*add)(map *self, int32_t key, char value);
+    mapElement (*add)(map *self, int32_t key, char value);
 };
 
-void mapAdd(map *self, int32_t key, char value)
+mapElement mapAdd(map *self, int32_t key, char value)
 {
+    int32_t existsAt = -1;
+    for (int i = 0; i < self->size; ++i)
+    {
+        if (key == self->keys[i])
+        {
+            existsAt = i;
+            break;
+        }
+    }
 
+    int32_t index;
+    mapElement result;
+    if (existsAt >= 0)
+    {
+        index = existsAt;
+        result.wasInserted = false;
+        result.key = self->keys[index];
+        result.value = self->values[index];
+
+        return result;
+    }
+
+    index = self->size;
+    self->keys = (int32_t *) realloc(self->keys, sizeof(int32_t) * (self->size + 1));
+    self->size++;
+    self->keys[index] = key;
+
+    self->values = (char *) realloc(self->values,self->size);
+    self->values[index] = value;
+
+    result.key = self->keys[index];
+    result.value = self->values[index];
+    result.wasInserted = true;
+
+    return result;
 }
 
-void mapInit (map *self, int64_t size)
+void mapInit (map *self, int32_t size)
 {
     self->keys = malloc(sizeof(int32_t) * size);
     self->values =  malloc(sizeof(char) * size);
@@ -31,5 +72,7 @@ int main(void)
 {
     map map;
     mapInit(&map, 10);
+
+    mapAdd(&map, 1, 'a');
     return 0;
 }
